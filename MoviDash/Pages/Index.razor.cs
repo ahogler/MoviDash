2281a4fd2d81ba516d5136c44569a8c2df170373
Alexandre Hogler <30938145+ahogler@users.noreply.github.com>
2022-04-07 08:55:15 -0300

    Ajustes

--- MoviDash/Pages/Index.razor.cs
+++ MoviDash/Pages/Index.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MoviDash.Services;
+using System;
 using System.Net.Http.Json;
 
 namespace MoviDash.Pages
@@ -7,9 +8,17 @@ namespace MoviDash.Pages
     public partial class Index
     {
         public List<Ticket> TicketsAbertosHoje { get; set; }
+        public List<Ticket> TicketsTabela { get; set; }
         public List<Ticket> TicketsTotalEmAberto { get; set; }
         public int AbertosHoje { get; set; }
         public int TotalEmAberto { get; set; }
+        public int AbertosEsseMes { get; set; }
+        public int AbertosHojeAindaAberto { get; set; }
+        public DateTime UltimaAtualizacao { get; set; }
+
+        private System.Threading.Timer? timer;
+
+        private bool _loading;
 
         [Inject]
         public ITicketService service { get; set; }
@@ -17,11 +26,25 @@ namespace MoviDash.Pages
         protected override async Task OnInitializedAsync()
         {
             base.OnInitialized();
-            TicketsAbertosHoje = (await service.AbertosHoje()).ToList();
-            AbertosHoje = TicketsAbertosHoje.Count();
+            UltimaAtualizacao = DateTime.Now;
 
-            TicketsTotalEmAberto = (await service.TotalAberto()).ToList();
-            TotalEmAberto = TicketsTotalEmAberto.Count();
+            timer = new System.Threading.Timer(async (object? stateInfo) =>
+            {
+                TicketsAbertosHoje = (await service.AbertosHoje()).ToList();
+                TicketsTabela = TicketsAbertosHoje.OrderByDescending(x => x.id).ToList();
+
+                AbertosHoje = TicketsAbertosHoje.Count();
+
+                AbertosHojeAindaAberto = TicketsAbertosHoje.Where(x => x.status != "Resolvido" && x.status != "Fechado").ToList().Count();
+
+                TicketsTotalEmAberto = (await service.TotalAberto()).ToList();
+                TotalEmAberto = TicketsTotalEmAberto.Count();
+
+                AbertosEsseMes = TicketsTotalEmAberto.Where(x => x.createdDate.Date.Month == DateTime.Now.Date.Month).ToList().Count();
+                UltimaAtualizacao = DateTime.Now;
+
+                StateHasChanged(); // NOTE: MUST CALL StateHasChanged() BECAUSE THIS IS TRIGGERED BY A TIMER INSTEAD OF A USER EVENT
+            }, new System.Threading.AutoResetEvent(false), 30000, 30000); // fire every 2000 milliseconds
         }
     }
     public class Ticket
