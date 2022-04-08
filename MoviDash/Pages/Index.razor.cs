f298141e236fe348191211f5b0a8bf2ce9e063f4
Alexandre Hogler <30938145+ahogler@users.noreply.github.com>
2022-04-08 00:40:56 -0300

    Ajuste ABA Por Agente

--- MoviDash/Pages/Index.razor.cs
+++ MoviDash/Pages/Index.razor.cs
@@ -9,6 +9,11 @@ namespace MoviDash.Pages
     public partial class Index
     {
         MudTabs tabs;
+        public List<Ticket> TicketsAndre { get; set; }
+        public List<Ticket> TicketsAni { get; set; }
+        public List<Ticket> TicketsAde { get; set; }
+        public List<Ticket> TicketsEverton { get; set; }
+        public List<Ticket> TicketsConcluidosHoje { get; set; }
         public List<Ticket> TicketsAbertosHoje { get; set; }
         public List<Ticket> TicketsTabela { get; set; }
         public List<Ticket> TicketsTotalEmAberto { get; set; }
@@ -16,6 +21,7 @@ namespace MoviDash.Pages
         public int TotalEmAberto { get; set; }
         public int AbertosEsseMes { get; set; }
         public int AbertosHojeAindaAberto { get; set; }
+        public int TotalConcluidosHoje { get; set; }
         public DateTime UltimaAtualizacao { get; set; }
 
         private System.Threading.Timer? timer;
@@ -32,6 +38,12 @@ namespace MoviDash.Pages
             base.OnInitialized();
             UltimaAtualizacao = DateTime.Now;
 
+            TicketsAndre = new List<Ticket>();
+            TicketsAni = new List<Ticket>();
+            TicketsAde = new List<Ticket>();
+            TicketsEverton = new List<Ticket>();
+            TicketsConcluidosHoje = new List<Ticket>();
+
             timer = new System.Threading.Timer(async (object? stateInfo) =>
             {
                 _loading = true;
@@ -47,12 +59,20 @@ namespace MoviDash.Pages
                 TicketsTotalEmAberto = (await service.TotalAberto()).ToList();
                 TotalEmAberto = TicketsTotalEmAberto.Count();
 
+                TicketsConcluidosHoje = (await service.ConcluidosHoje()).ToList();
+                TotalConcluidosHoje = TicketsConcluidosHoje.Count();
+
+                TicketsAndre = TicketsTotalEmAberto.Where(x => x.owner?.id == "1184814735").ToList();
+                TicketsAde = TicketsTotalEmAberto.Where(x => x.owner?.id == "1360469349").ToList();
+                TicketsEverton = TicketsTotalEmAberto.Where(x => x.owner?.id == "1933648929").ToList();
+                TicketsAni = TicketsTotalEmAberto.Where(x => x.owner?.id == "145414126").ToList();
+
                 AbertosEsseMes = TicketsTotalEmAberto.Where(x => x.createdDate.Date.Month == DateTime.Now.Date.Month).ToList().Count();
                 UltimaAtualizacao = DateTime.Now;
 
                 _loading = false;
                 StateHasChanged(); // NOTE: MUST CALL StateHasChanged() BECAUSE THIS IS TRIGGERED BY A TIMER INSTEAD OF A USER EVENT
-            }, new System.Threading.AutoResetEvent(false), 30000, 30000); // fire every 2000 milliseconds
+            }, new System.Threading.AutoResetEvent(false), 20000, 20000); // fire every 2000 milliseconds
 
             tabIndex = 0;
             timerTabs = new Timer(async (object? stateInfo) =>
@@ -80,7 +100,11 @@ namespace MoviDash.Pages
         public string category { get; set; }
         public DateTime createdDate { get; set; }
         public string subject { get; set; }
+        public string justification { get; set; }
         public int id { get; set; }
+        public Owner owner { get; set; }
+        public string urgency { get; set; }
+        public string ownerTeam { get; set; }
 
         public string TicketLink
         {
@@ -95,6 +119,39 @@ namespace MoviDash.Pages
                     return Color.Success;
             }
         }
+
+        public List<client> clients { get; set; }
+
+        public string Empresa
+        {
+            get
+            {
+                if (clients == null || clients[0].organization == null) return "";
+                return clients[0].organization.businessName;
+            }
+        }
+
+    }
+
+    public class client
+    {
+        public string businessName { get; set; }
+        public empresa organization { get; set; }
+        public class empresa
+        {
+            public string businessName { get; set; }
+        }
+    }
+
+    public class Owner
+    {
+        public string id { get; set; }
+        public int personType { get; set; }
+        public int profileType { get; set; }
+        public string businessName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string pathPicture { get; set; }
 
     }
 
--- MoviDash/Services/TicketService.cs
+++ MoviDash/Services/TicketService.cs
@@ -5,7 +5,7 @@ namespace MoviDash.Services
 {
     public class TicketService : ITicketService
     {
-        const string URL_BASE = "https://api.movidesk.com/public/v1/tickets?token=65a837f3-8d4c-4dc8-b755-536dcf00dbab&$select=id,subject,createdDate,category,status";
+        const string URL_BASE = "https://api.movidesk.com/public/v1/tickets?token=65a837f3-8d4c-4dc8-b755-536dcf00dbab&$select=id,subject,createdDate,category,status,urgency,justification,ownerTeam";
         private readonly HttpClient httpClient;
 
         public TicketService(HttpClient httpClient)
@@ -21,7 +21,7 @@ namespace MoviDash.Services
 
         public async Task<IEnumerable<Ticket>> ConcluidosHoje()
         {
-            var url = MontarUrl(StatusEmAberto());
+            var url = MontarUrl(FiltroConcluidosHoje());
             return await httpClient.GetFromJsonAsync<IEnumerable<Ticket>>(url);
         }
 
@@ -32,11 +32,17 @@ namespace MoviDash.Services
         }
         public static string Hoje()
         {
-            return $"createdDate gt {DateTime.Today.ToUniversalTime().ToString("o")}";
+            return $"(urgency ne 'Projetos') and createdDate gt {DateTime.Today.ToUniversalTime().ToString("o")}";
         }
+
+        public static string FiltroConcluidosHoje()
+        {
+            return $"(urgency ne 'Projetos') and resolvedIn gt {DateTime.Today.ToUniversalTime().ToString("o")} and (status eq 'Resolvido' or status eq 'Fechado')";
+        }
+
         public static string StatusEmAberto()
         {
-            return "(status eq 'Não concluído' or status eq 'Em atendimento' or status eq 'Novo' or status eq 'Parado')";
+            return "(urgency ne 'Projetos') and (status eq 'Não concluído' or status eq 'Em atendimento' or status eq 'Novo' or status eq 'Parado')";
         }
         private static string MontarUrl(string filtros)
         {
